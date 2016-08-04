19e82c1a9c65807bf55be43099145f52ced6e42c
Naka <Naka@10.21.77.134>
2016-08-04 16:47:23 -0500

    Changed TheaterTemplate

--- TheaterJSF/src/java/beans/HomeBean.java
+++ TheaterJSF/src/java/beans/HomeBean.java
@@ -11,12 +11,15 @@ package beans;
  */
 import XMLparsing.Movie;
 import ejb.theaterEJB;
+import entity.Checkout;
 import entity.Theater;
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 
 /**
@@ -24,11 +27,13 @@ import javax.faces.context.FacesContext;
  * @author JamesEich
  */
 @Named(value = "homeBean")
-@RequestScoped
-public class HomeBean{
+@SessionScoped
+public class HomeBean implements Serializable{
     @EJB
     private theaterEJB theaterEJB;
     private Theater theater;
+    private Checkout checkout;
+    private Movie movie;
     private String zip;
     private String theaterId;
     private String selectedTime; 
@@ -80,6 +85,40 @@ public class HomeBean{
         this.selectedTime = selectedTime;
     }
     
+    
+    
+    
+    //Checkout Object
+    public Checkout getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(Checkout checkout) {
+        this.checkout = checkout;
+    }
+    
+    public String copyCheckout(Checkout ch){
+        this.checkout = ch;
+        return "Checkout.xhtml";
+    }
+
+    
+    
+    
+    
+    
+    //Movie Object
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+    public String copyMovie(Movie mov){
+        this.movie = mov;
+        return "Checkout.xhtml";
+    }
     
     
     
